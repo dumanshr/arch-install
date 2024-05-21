9ff9824fef2c311aae4628926754dddd1c672ba2
Duman <phyduman@gmail.com>
2024-05-20 21:20:24 -0400

    downloader added

---
+++ download-installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/arch-install.sh
+curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/after-chroot.sh
+curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/install-gnome.sh
+curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/envfile.sample
+
+
