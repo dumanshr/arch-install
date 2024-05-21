f4ea69fb06dda747046f804c9c6e086b01d7c915
Duman <phyduman@gmail.com>
2024-05-21 13:28:52 -0400

    rsync recursive, typo in chmod

--- assets/config-download.sh
+++ assets/config-download.sh
@@ -8,10 +8,11 @@ mkdir -p $HOME/.local/share/applications/
 
 git clone https://github.com/dumanshr/dotfiles.git
 cd dotfiles
+
 cp .bashrc $HOME/
-rsync .local/ $HOME/.local/
-rsync .config/ $HOME/.config/
-chomd +x $HOME/.local/bin/*
+rsync -r .local/ $HOME/.local/
+rsync -r .config/ $HOME/.config/
+chmod +x $HOME/.local/bin/*
 
 
 touch $HOME/.local/share/applications/{assistant,avahi-discover,bssh,bvnc,designer,java-java-openjdk,jconsole-java-openjdk,jshell-java-openjdk,linguist,lstopo,nvim,org.gnome.Extensions,qdbusviewer,qv4l2,qvidcap,xdvi}.desktop
