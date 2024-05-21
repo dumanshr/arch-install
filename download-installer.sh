adf767456311a73b515cb7d305fb4f3149ee2079
Duman <phyduman@gmail.com>
2024-05-20 22:27:46 -0400

    download-installer typo fixed

--- download-installer.sh
+++ download-installer.sh
@@ -2,10 +2,11 @@
 
 curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/arch-install.sh
 curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/after-chroot.sh
-curl -O https://raw.githubusercontent.com/dumanshr/arch-install/mastafter-chroot.sher/install-gnome.sh
+curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/install-gnome.sh
 curl -O https://raw.githubusercontent.com/dumanshr/arch-install/master/envfile.sample
 
 
 chmod +x arch-install.sh
 chmod +x after-chroot.sh
 chmod +x install-gnome.sh
+cp envfile.sample envfile
