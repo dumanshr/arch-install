313a3305dd30ab2eabd4363d09a5df0325a191cc
Duman <phyduman@gmail.com>
2024-06-09 16:47:56 -0400

    removed chaotic-aur addtion before restart to after restart

--- install-gnome.sh
+++ install-gnome.sh
@@ -16,6 +16,7 @@ pacman -S --needed --noconfirm --overwrite \* \
 	xdg-user-dirs-gtk \
 	xdg-desktop-portal-gnome \
 	gnome-keyring \
+	gnome-text-editor \
 	gvfs-afc gvfs-goa gvfs-google gvfs-onedrive\
 	gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb \
 	evince totem loupe snapshot \
@@ -64,23 +65,9 @@ systemctl enable gdm
 systemctl enable switcheroo-control
 systemctl enable bluetooth
 
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo '[chaotic-aur]'>>/etc/pacman.conf
-echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
-
-pacman -Syyu
-
-pacman -S --needed --noconfirm --overwrite \* brave-bin sublime-text-4
-
 mkdir -p /usr/share/nautilus-python/extensions
 curl -o /usr/share/nautilus-python/extensions/open-in-terminal.py https://raw.githubusercontent.com/dumanshr/arch-install/master/assets/open-in-terminal.py
 
-
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=16NGDYZo44nQTO7ZNm0eAI_NL8CkEB6_o' -O /tmp/win11-fonts.tar.zst
-pacman -U --noconfirm '/tmp/win11-fonts.tar.zst'
 
 curl -o /tmp/config-download.sh https://raw.githubusercontent.com/dumanshr/arch-install/master/assets/config-download.sh
 chmod +x /tmp/config-download.sh
@@ -88,8 +75,6 @@ NORMAL_USER=$(id -un 1000)
 cd /home/${NORMAL_USER}
 su ${NORMAL_USER} /tmp/config-download.sh
 
-
-fc-cache --force
 
 
 
